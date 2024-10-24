0518c9fbb6f941419c099cfd44b447685af8d742
tobiaswindingstad <tobias.windingstad@gmail.com>
2024-10-24 14:50:58 +0200

    fix class and cabin corelation function and add plot

--- wrangling.R
+++ wrangling.R
@@ -1,24 +1,28 @@
-class_cabin_data <- data %>% 
-  select(Pclass, Cabin) %>%
-  filter(!is.na(Cabin)) %>%
-  mutate(cabin_prefix = substr(Cabin, 1, 1)) %>% 
-  select(Pclass, cabin_prefix)
 
-cabin_counts_one <- class_cabin_data %>%
-  filter(Pclass == "1") %>% 
-  count(cabin_prefix)
-glimpse(cabin_counts_one)
+class_cabin_correlation <- function() {
+  # Create tibble with all known Cabin and PClass
+  class_cabin_data <- data %>% 
+    select(Pclass, Cabin) %>%
+    filter(!is.na(Cabin)) %>%
+    mutate(cabin_prefix = substr(Cabin, 1, 1)) %>% 
+    select(Pclass, cabin_prefix)
+  
+  view(class_cabin_data)
+  
+  # Count cabin prefix for each class
+  cabin_counts <- class_cabin_data %>%
+    group_by(Pclass, cabin_prefix) %>%
+    count() %>%
+    ungroup() 
+  
+  view(cabin_counts)
+  
+  # Plot to see correlation between Class and Cabin prefix
+  plot <- ggplot(cabin_counts, aes(x = cabin_prefix, y = n, fill = factor(Pclass))) +
+    geom_bar(stat = "identity", position = "dodge") +  # Use dodged bars for each class
+    labs(x = "Cabin Prefix", y = "Count", title = "Count of Passengers by Cabin Prefix and Pclass") +
+    theme_minimal()
+  
+  plot
+}
 
-cabin_counts_two <- class_cabin_data %>%
-  filter(Pclass == "2") %>% 
-  count(cabin_prefix)
-glimpse(cabin_counts_two)
-
-cabin_counts_three <- class_cabin_data %>%
-  filter(Pclass == "3") %>% 
-  count(cabin_prefix)
-glimpse(cabin_counts_three)
-
-#plot <- ggplot(class_cabin_data, aes(x = cabin_prefix, y = ))
-
-View(class_cabin_data)

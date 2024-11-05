16abb042dd954fadb005c1b827dd81b095f852f7
orjanhsy <orjanhsy@gmail.com>
2024-11-05 15:30:51 +0100

    add prep_data function

--- wrangling.R
+++ wrangling.R
@@ -1,36 +1,32 @@
 
-class_cabin_correlation <- function() {
-  # Create tibble with all known Cabin and PClass
-  class_cabin_data <- data %>% 
-    select(Pclass, Cabin) %>%
-    filter(!is.na(Cabin)) %>%
-    mutate(cabin_prefix = substr(Cabin, 1, 1)) %>% 
-    select(Pclass, cabin_prefix)
-  
-  view(class_cabin_data)
-  
-  # Count cabin prefix for each class
-  cabin_counts <- class_cabin_data %>%
-    group_by(Pclass, cabin_prefix) %>%
-    count() %>%
-    ungroup() 
-  
-  view(cabin_counts)
-  
-  # Plot to see correlation between Class and Cabin prefix
-  plot <- ggplot(cabin_counts, aes(x = cabin_prefix, y = n, fill = factor(Pclass))) +
-    geom_bar(stat = "identity", position = "dodge") +  # Use dodged bars for each class
-    labs(x = "Cabin Prefix", y = "Count", title = "Count of Passengers by Cabin Prefix and Pclass") +
-    theme_minimal()
-  
- 
-  
-  #Chi_squared to see association between Class and Cabin prefix
-  contingency_table <- xtabs(n ~ Pclass + cabin_prefix, data = cabin_counts)
-  print(contingency_table)
-  chi_squared_result <- chisq.test(contingency_table)
-  print(chi_squared_result)
-  
-  plot
+# Dependencies
+dependencies <- c("tidyverse", "readr")
+for (pkg in dependencies) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg)
+  }
+  library(pkg, character.only = TRUE)
 }
 
+
+prep_data <- function(na = FALSE) {
+  # Importing the data
+  path <- here::here("data", "Titanic-Dataset.csv")
+  data <- read_csv(path)
+  if (na) {
+    return (data)
+  }
+  
+  data <- handle_na_age(data)
+  view(data)
+  return (data)
+}
+
+handle_na_age <- function(data) {
+  median_age <- median(data$Age, na.rm = TRUE)
+  # print(median_age)
+  data %>%
+    mutate(Age = ifelse(is.na(Age), median_age, Age))
+}
+
+print(handle_na_age(prep_data()))

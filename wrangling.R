9981a6a50c786ed9f110ead07c89c9a809ab8fee
tobiaswindingstad <tobias.windingstad@gmail.com>
2024-11-05 16:01:35 +0100

    ops main. add column for title

--- wrangling.R
+++ wrangling.R
@@ -9,24 +9,38 @@ for (pkg in dependencies) {
 }
 
 
+handle_na_age <- function(data) {
+  median_age <- median(data$Age, na.rm = TRUE)
+  data <- data %>%
+    mutate(Age = ifelse(is.na(Age), median_age, Age))
+  return(data)
+}
+
+#function that extracts title and adds a new "Title" column
+extract_title <- function(data){
+  data <- data %>%
+    mutate(Title = sub(".*,\\s*(\\w+)\\..*", "\\1", Name))
+  return(data)
+}
+
 prep_data <- function(na = FALSE) {
   # Importing the data
   path <- here::here("data", "Titanic-Dataset.csv")
   data <- read_csv(path)
-  if (na) {
-    return (data)
-  }
+  if (na) return (data)
   
   data <- handle_na_age(data)
+  data <- extract_title(data)
   view(data)
   return (data)
 }
 
-handle_na_age <- function(data) {
-  median_age <- median(data$Age, na.rm = TRUE)
-  # print(median_age)
-  data %>%
-    mutate(Age = ifelse(is.na(Age), median_age, Age))
-}
+data <- prep_data()
+print(data)
 
-print(handle_na_age(prep_data()))
+
+
+
+
+
+

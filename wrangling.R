8b6580743a2b8c351cd434baef081d675b782bf3
orjanhsy <orjanhsy@gmail.com>
2024-11-06 13:42:20 +0100

    make na-age an estimate based on siblings/spuse

--- wrangling.R
+++ wrangling.R
@@ -12,9 +12,30 @@ for (pkg in dependencies) {
 handle_na_age <- function(data) {
   median_age <- median(data$Age, na.rm = TRUE)
   data <- data %>%
-    mutate(Age = ifelse(is.na(Age), median_age, Age))
+    mutate(Age = ifelse(is.na(Age), ifelse((SibSp > 0) & (Parch == 0), getSibSpAge(Name, data), median_age), Age))
   return(data)
 }
+
+# gets the mean age of sibling(s) and/or spouse(s?), helpful in cases where age is N/A. 
+# The function assumes people with the same last name are siblings.
+# In cases where children/parents, or unrelated people with the same last name are present
+# this will result in a less accurate estimate of the persons age.
+getSibSpAge <- function(name, data) {
+  last_name <- get_last_name(name)
+  # print(paste("Getting siblings for last name:", last_name))
+  
+  sib_sp <- filter(group_by(data, Name), get_last_name(Name) == last_name)
+  print(sib_sp)
+  
+  estimated_age <- round(mean(sib_sp$Age, na.rm = TRUE))
+  return (estimated_age)
+}
+
+get_last_name <- function(name) {
+  last_name <- strsplit(name, ',')[[1]][1]
+  return (last_name)
+}
+
 
 #function that extracts title and adds a new "Title" column
 extract_title <- function(data){
@@ -28,16 +49,13 @@ prep_data <- function(na = FALSE) {
   path <- here::here("data", "Titanic-Dataset.csv")
   data <- read_csv(path)
   if (na) return (data)
-  
   data <- handle_na_age(data)
   data <- extract_title(data)
-  view(data)
   return (data)
 }
 
 data <- prep_data()
-print(data)
-
+view(data)
 
 
 

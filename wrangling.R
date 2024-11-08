4967afacf787b44999537ae4bf923d435a80b058
orjanhsy <orjanhsy@gmail.com>
2024-11-08 16:46:46 +0100

    create dummy data and untuned models

--- wrangling.R
+++ wrangling.R
@@ -1,6 +1,6 @@
 
 # Dependencies
-dependencies <- c("tidyverse", "readr")
+dependencies <- c("tidyverse", "readr", "rsample", "tidymodels", "recipes", "glmnet", "ranger")
 for (pkg in dependencies) {
   if (!require(pkg, character.only = TRUE)) {
     install.packages(pkg)
@@ -36,7 +36,7 @@ create_median_fare_plot <- function(median_fares, avarage_NA){
 #set NA Embarked to closest median
 set_port <- function(data){
   median_fares_for_port <- get_median_fare_by_port(data)
-  mean_fare_NA <- avarage_NA_port(data)
+  mean_fare_NA <- avarage_na_port(data)
   
   closest_port <- median_fares_for_port %>%
     mutate(diff = abs(median_fare - mean_fare_NA)) %>%
@@ -111,13 +111,92 @@ wrangle_data <- function(na = FALSE) {
 }
 
 create_dummy_data <- function(data) {
+  dummy_data <- data %>%
+    mutate(across(where(is.character), as.factor))
   
+  split <- initial_split(dummy_data, prop = 0.8)
+  t_train <- training(split)
+  t_test <- testing(split)
+  
+  print(t_train)
+  
+  dummies <- recipe(Survived ~ ., data = t_train) %>%
+    step_dummy(all_factor_predictors()) %>%
+    prep(training = t_train)
+  
+  dummy_train <- bake(dummies, new_data = t_train)
+  dummy_test <- bake(dummies, new_data = t_test)
+  
+  return(list(t_train = dummy_train, t_test = dummy_test))
 }
 
-data <- wrangle_data()
-na_data <- wrangle_data(na = TRUE)
-view(data)
+linear_regression_model <- function(t_train) {
+  model <- linear_reg() %>%
+    fit(Survived ~., data = t_train)
+}
 
+lasso_model <- function(t_train) {
+  lso <- linear_reg(
+    penalty = 0.1,
+    mixture = 1
+  ) %>%
+    set_engine("glmnet") %>%
+    fit(Survived ~., data = t_train)
+}
 
+random_forest_model <- function(t_train) {
+  rf <- rand_forest(
+    mode = "regression",
+    engine = "ranger",
+  ) %>%
+    fit(Survived ~., data = t_train)
+}
 
+xgboost_model <- function(t_train){
+  xgbm <- boost_tree() %>%
+    set_engine("xgboost") %>%
+    set_mode("regression") %>%
+    fit(Survived ~., data = t_train)
+}
 
+main <- function() {
+  # prepare data for models
+  data <- wrangle_data()
+  na_data <- wrangle_data(na = TRUE)
+  
+  model_data <- create_dummy_data(data)
+  t_train <- model_data$t_train
+  t_test <- model_data$t_test
+  
+  # -- Models --
+  # OLS
+  ols_model <- linear_regression_model(t_train)
+  print("Trained OLS model:")
+  print(summary(ols_model))
+  print(alias(ols_model$fit))
+
+  ols_pred <- predict(ols_model, new_data = t_test, rankdeficient = 'NA')
+  
+  # LASSO
+  lso_model <- lasso_model(t_train)
+  print("Trained LASSO model:")
+  print(summary(lso_model))
+
+  lso_pred <- predict(lso_model, new_data = t_test)
+
+  # Random Forest 
+  rf_model <- random_forest_model(t_train)
+  print("Trained random forest model:")
+  print(summary(rf_model))
+
+  rf_pred <- predict(rf_model, new_data = t_test)
+  
+  # Gradient Boosting Tree
+  xgb_model <- xgboost_model(t_train)
+  print("Trained Gradient Boosting Tree model:")
+  print(summary(xgb_model))
+  
+  xgb_pred <- predict(xgb_model, new_data = t_test)
+}
+
+main()

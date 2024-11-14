dbd4530437b11bacd4a6d86234cc0e221907bf74
oerjan <orjanhsy@gmail.com>
2024-11-14 12:13:59 +0100

    add separate spec functions for each model

--- code/models/models.R
+++ code/models/models.R
@@ -26,3 +26,60 @@ xgboost_model <- function(t_train){
     set_mode("regression") %>%
     fit(Survived ~., data = t_train)
 }
+
+tune <- function(model, t_train, type) {
+  spec <- switch(
+    type,
+    "lasso" = lasso_spec(tune())
+  )
+}
+
+# -- Model specs, all parameters apart from trees are tunable. --
+
+# ols cannot be tuned as it has no hyperparams, default engine of linear_reg is lm.
+ols_spec <- function() {
+  return (linear_reg())
+}
+
+lasso_spec <- function(penalty) {
+  return (
+    linear_reg(
+      engine = "glmnet",
+      mode = "classification",
+      penalty = penalty,
+      mixture = 1 # constant required for pure lasso
+    ) 
+  )
+}
+
+rf_spec <- function(mtry = NULL, trees = 500,  min_n = NULL) {
+  return (
+    rand_forest(
+      mtry = mtry, 
+      trees = trees,
+      min_n = min_n,
+      mode = "classification",
+      engine = "ranger",
+    )   
+  )
+}
+
+xgb_spec <- function(trees = 500, depth = NULL, learn_rate = NULL, loss_reduction = NULL, min_n = NULL) {
+  return (
+    boost_tree(
+      engine = "xgboost",
+      mode = "classification", 
+      tree_depth = depth,
+      learn_rate = learn_rate,
+      loss_reduction = loss_reduction,
+      min_n = min_n
+    )
+  )
+}
+
+
+
+
+
+
+

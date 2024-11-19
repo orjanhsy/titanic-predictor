b84f5f4375ace413a01bf4d380ba848a740adbb5
tobiaswindingstad <tobias.windingstad@gmail.com>
2024-11-19 13:38:05 +0100

    add embarked c add app outline

---
+++ app.R
@@ -0,0 +1,108 @@
+
+
+library(shiny)
+library(bslib)
+
+ui <- fluidPage(
+  theme = bs_theme(
+    version = 4,
+    bootswatch = "minty"          
+  ),
+  
+  titlePanel("Kjøp billett til Titanic"),
+  sidebarLayout(
+    sidebarPanel(
+      h3("Fyll ut billettinformasjonen"),
+      
+      selectInput(
+        inputId = "title",
+        label = "Velg tittel:",
+        choices = c("Herr", "Fru", "Frøken", "Mester", "Don", "Dr.", "Sir", "Oberst", "Grevinne"),
+        selected = "Herr"
+      ),
+      
+      textInput(
+        inputId = "name",
+        label = "Skriv inn navnet ditt:",
+        placeholder = "Skriv navnet ditt her"
+      ),
+      
+      numericInput(
+        inputId = "age",
+        label = "Oppgi alder:",
+        value = 30,
+        min = 1,
+        max = 120
+      ),
+      
+      selectInput(
+        inputId = "gender",
+        label = "Velg kjønn:",
+        choices = c("Mann", "Kvinne"),
+        selected = "Mann"
+      ),
+      
+      selectInput(
+        inputId = "port",
+        label = "Hvor reiser du fra?",
+        choices = c("Southampton", "Cherbourg", "Queenstown"),
+        selected = "Southampton"
+      ),
+      
+      selectInput(
+        inputId = "pclass",
+        label = "Velg billettklasse:",
+        choices = c("1. klasse - 10.000kr", "2. klasse - 5.000kr", "3. klasse - 2.500kr"),
+        selected = "1. klasse"
+      ),
+      
+      sliderInput(
+        inputId = "siblings",
+        label = "Reiser du med søsken?",
+        min = 0,
+        max = 10,
+        value = 0
+      ),
+      
+      sliderInput(
+        inputId = "children",
+        label = "Reiser du med barn?",
+        min = 0,
+        max = 10,
+        value = 0
+      ),
+      
+      radioButtons(
+        inputId = "partner",
+        label = "Reiser du med ektefelle eller kjæreste?",
+        choices = c("Ja", "Nei"),
+        selected = "Nei"
+      ),
+      
+      radioButtons(
+        inputId = "parents",
+        label = "Reiser du med foreldre?",
+        choices = c("Nei", "En", "Begge"),
+        selected = "Nei"
+      ),
+      
+      
+      actionButton(
+        inputId = "submit_btn",
+        label = "Kjøp billett!"
+      )
+    ),
+    
+    mainPanel(
+      h3("Fyll ut informasjonen for å kjøpe billett."),
+      br(),
+      textOutput("confirmation")
+    )
+  )
+)
+server <- function(input, output) {
+  # Server logic (not needed for now)
+}
+
+shinyApp(ui = ui, server = server)
+
--- code/models/model_data.R
+++ code/models/model_data.R
@@ -16,11 +16,14 @@ create_dummy_data <- function(data) {
   
   dummies <- recipe(Survived ~ ., data = t_train) %>%
     update_role(Survived, new_role = "outcome") %>%
-    step_dummy(all_factor_predictors(), -all_outcomes()) %>%
+    update_role(Embarked, new_role = "predictor") %>%
+    step_dummy(all_factor_predictors(), -all_outcomes(), one_hot = TRUE) %>%
     prep(training = t_train)
   
   dummy_train <- bake(dummies, new_data = t_train)
   dummy_test <- bake(dummies, new_data = t_test)
+  
+
   
   return(list(t_train = dummy_train, t_test = dummy_test))
 }
